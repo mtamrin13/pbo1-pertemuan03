9c3fba741eb892230e7900ac8a60ae40c5c6476e
mtamrin13 <muhammadtamrin13@gmail.com>
2020-05-03 10:24:03 -0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Tamrin";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
